3e23c77bbbb1d2e4927591092905a3e6a1bf210a
Jose Contreras <jose.contreras.godoy@gmail.com>
2020-06-23 01:04:30 -0500

    Making Queue class "generic"

--- Queue/queue.h
+++ Queue/queue.h
@@ -1,7 +1,51 @@
 #ifndef QUEUE_H_
 #define QUEUE_H_
 
+#include <iostream>
+
+/*Helper class for queue node*/
+template <class T>
+class queue_node{
+    private:
+        T data;
+        queue_node* next;
+    public:
+        queue_node(T);
+
+        void set_data(T);
+        void set_next(queue_node*);
+        T get_data(void);
+        queue_node* get_next(void);
+};
+
+template <class T>
+queue_node<T>::queue_node(T data){
+    this->data = data;
+    this->next = NULL;
+}
+
+template <class T>
+void queue_node<T>::set_data(T data){
+    this->data = data;
+}
+
+template <class T>
+void queue_node<T>::set_next(queue_node* next){
+    this->next = next;
+}
+
+template <class T>
+T queue_node<T>::get_data(void){
+    return this->data;
+}
+
+template <class T>
+queue_node<T>* queue_node<T>::get_next(void){
+    return this->next;
+}
+
 /*Queue class*/
+template <class T>
 class Queue{
     private:
         int size;
@@ -9,11 +53,55 @@ class Queue{
         void* tail;
     public:
         Queue();
-        void enqueue(int);
-        int dequeue(void);
-        int peek(void);
+        void enqueue(T);
+        T dequeue(void);
+        T peek(void);
         int getSize(void){return this->size;}
 };
+
+
+template <class T>
+Queue<T>::Queue(){
+    this->size = 0;
+    this->head = this->tail = NULL;
+}
+
+template <class T>
+void Queue<T>::enqueue(T data){
+    queue_node<T>* temp =  new queue_node<T> (data);
+    
+    if(this->size==0)
+        this->head = this->tail = temp;
+    else
+    {
+        ((queue_node<T>*)this->tail)->set_next(temp);
+        this->tail = temp;
+    }
+    this->size++;
+}
+
+template <class T>
+T Queue<T>::dequeue(void){
+
+    T ret;
+    if(this->size>0){
+        queue_node<T>* temp;
+        temp = (queue_node<T>*)this->head;
+        this->head = temp->get_next();
+        
+        ret = temp->get_data();
+        delete temp;
+        this->size--;
+    }
+
+    return ret;
+}
+
+template <class T>
+T Queue<T>::peek(void){
+    return ((queue_node<T>*)this->head)->get_data();
+}
+
 
 
 #endif /*QUEUE_H_*/
--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "Queue/queue.h"
 #include <iostream>
+#include <string>
+
 /*Main function*/
+using namespace std;
+
 int main(void){
 
-    Queue q;
+    Queue<int> q;
+    Queue<double> q2;
+    Queue<string> qs;
     
     q.enqueue(5);
     q.enqueue(10);
@@ -12,8 +18,29 @@ int main(void){
     q.enqueue(25);
     
     while(q.getSize())
-        std::cout << q.dequeue() << std::endl;
+        cout << q.dequeue() << endl;
 
+    cout << endl;
+    
+    q2.enqueue(2.455);
+    q2.enqueue(4.35);
+    q2.enqueue(0.23546);
+    q2.enqueue(5.3546);
+    q2.enqueue(10.00201);
+    
+    while(q2.getSize())
+        cout << q2.dequeue() << endl;
+
+    cout << endl;
+
+    qs.enqueue("Holi");
+    qs.enqueue("Este es");
+    qs.enqueue("Un mensaje");
+    qs.enqueue("Del futuro");
+    qs.enqueue("Adios");
+    
+    while(qs.getSize())
+        cout << qs.dequeue() << endl;
     return 0;
 }
 
